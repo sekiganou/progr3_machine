a16ff47879791351523445734a83fa0efd12999c
Alessio Bagno <alessiobagno@gmail.com>
2025-09-08 22:40:11 +0200

    add comment to bring commit forward

--- java_reflection/src/java_reflection_main.java
+++ java_reflection/src/java_reflection_main.java
@@ -2,13 +2,14 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class java_reflection_main {
+    // aggiungo commento per portare avanti il commit
     private static Polygon instantiatePolygon() {
         System.out.println("Choose a polygon: ");
 
         Scanner sc = new Scanner(System.in);
         String selection = sc.nextLine();
         Polygon polygon = null;
-        try{
+        try {
             String className = selection.substring(0, 1).toUpperCase() + selection.substring(1);
             Class<?> clazz = Class.forName(className);
             polygon = (Polygon) clazz.getDeclaredConstructor().newInstance();
@@ -16,7 +17,7 @@ public class java_reflection_main {
             e.printStackTrace();
         }
 
-        if(polygon == null){
+        if (polygon == null) {
             System.out.println("Invalid polygon");
             return null;
         }
@@ -26,28 +27,27 @@ public class java_reflection_main {
 
         System.out.println("With " + attributes.length + " attributes: " + Arrays.toString(attributes));
 
-        if(attributes.length == 0)
+        if (attributes.length == 0)
             return null;
 
         float[] attributesValue = new float[attributes.length];
         int index = 0;
-        for(String attribute : attributes){
+        for (String attribute : attributes) {
             System.out.println("Attribute " + attribute);
             attributesValue[index] = sc.nextFloat();
             index++;
         }
         polygon.setAttributes(attributesValue);
 
-        return  polygon;
+        return polygon;
     }
-
 
     public static void main(String[] args) {
         int i = 3;
 
         Geometries geometries = new Geometries();
         for (int j = 0; j < i; j++) {
-            boolean  added = geometries.addPolygon(instantiatePolygon());
+            boolean added = geometries.addPolygon(instantiatePolygon());
             System.out.println("Polygon " + (added ? "not " : "") + "added");
         }
 
