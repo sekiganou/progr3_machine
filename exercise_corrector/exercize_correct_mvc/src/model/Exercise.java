a59799f128a5453cbac63033f39e48ccf41b98b1
Alessio Bagno <alessiobagno@gmail.com>
2025-09-17 22:58:33 +0200

    remove exercise corrector and init activity counter

--- exercise_corrector/exercize_correct_mvc/src/model/Exercise.java
+++
@@ -1,7 +0,0 @@
-package model;
-
-public record Exercise(String text, int solution) {
-    public boolean isAnswerCorrect(int answer){
-        return answer == solution;
-    }
-}
--- exercise_corrector/src/ExerciseCorrectorMain.java
+++
@@ -1,4 +0,0 @@
-public class ExerciseCorrectorMain {
-    public static void main(String[] args) {
-    }
-}
